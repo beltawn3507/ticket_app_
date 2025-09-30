25c589a75b4f369af970113dac2d9057e0def862
beltawn3507 <152001888+beltawn3507@users.noreply.github.com>
2025-09-30 15:02:21 +0530

    Error Handler

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,15 +1,27 @@
 import express from 'express'
+import 'express-async-errors'
 import {json} from 'body-parser'
+
+// router 
+import { currentUserRouter } from './routes/current-user';
+import { signupRouter } from './routes/signup';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { errorHandler } from './middlewares/error-handler';
 
 const app=express();
 app.use(json());
 
-app.get('/api/users/currentuser',(req,res)=>{
-    res.send('hello');
-})
+// routes for this
+app.use(currentUserRouter);
+app.use(signupRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(errorHandler);
+
 
 app.listen(3000,()=>{
-    console.log("listening on port 3000 !!")
+    console.log("listening on port 3000 .!")
 });
 
 
